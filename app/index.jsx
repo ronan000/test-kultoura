e3ec789b51dc22cf28c98216ff038119de296eb2
ronan000 <64355509+ronan000@users.noreply.github.com>
2024-05-12 23:13:11 +0800

    Add files via upload

---
+++ app/index.jsx
@@ -0,0 +1,53 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import {ScrollView, Text, View, Image } from 'react-native';
+import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants';
+import CustomButton from './components/CustomButton';
+
+
+export default function App() {
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{height: '100%'}}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+          source={images.logo}
+          className="w-[130x] h-[84px]"
+          resizeMode="contain"
+          />
+
+          <Image
+          source={images.cards}
+          className="max-w-[380px] w-full h-[300px]"
+          resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless{"\n"}
+              Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless
+            Exploration with Aora
+          </Text>
+
+          <CustomButton></CustomButton>
+
+        </View>
+      </ScrollView>
+      
+      
+    </SafeAreaView>
+  );
+}
